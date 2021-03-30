34d507b1627e48a2d520b2c5d39abdd28d5e5009
Gururaj Pandurangi <panduran@codeaurora.org>
2021-03-29 17:43:03 -0700

    qcacld-3.0: Fix tx power calculation
    Fix the TPE IE population for SAP. The total power and PSD power
    are interchanged in TPE populate API.
    Also, fix the TPC power calculation logic STA in non-PSD case when
    TPE IE is present.
    Populate current AP power type for SAP as well.
    
    Change-Id: I0fb9b9318d1b5a3e1c8071714b7e3f6129828c92
    CRs-Fixed: 2910658

--- drivers/staging/qcacld-3.0/core/mac/src/sys/legacy/src/utils/src/parser_api.c
+++ drivers/staging/qcacld-3.0/core/mac/src/sys/legacy/src/utils/src/parser_api.c
@@ -345,9 +345,9 @@ populate_dot11f_tx_power_env(struct mac_context *mac,
 
 		bw_val = wlan_reg_get_bw_value(chan_width);
 		bw_threshold = 20;
-		power_for_bss = reg_power + 13;
+		power_for_bss = eirp_power + 13;
 
-		while ((eirp_power > power_for_bss) &&
+		while ((reg_power > power_for_bss) &&
 		       (bw_threshold < bw_val)) {
 			bw_threshold = 2 * bw_threshold;
 			power_for_bss += 3;
@@ -363,18 +363,11 @@ populate_dot11f_tx_power_env(struct mac_context *mac,
 			tpe_ptr->max_tx_pwr_interpret = 2;
 			tpe_ptr->max_tx_pwr_category = 0;
 			tpe_ptr->num_tx_power = num_tx_power;
-			for (count = 0, bw_val = 20; count < num_tx_power;
-			     count++, bw_val += 20) {
-				if (bw_val >= bw_threshold)
-					tpe_ptr->tx_power[count] =
-								eirp_power * 2;
-				else
-					tpe_ptr->tx_power[count] =
-								reg_power * 2;
+			for (count = 0; count < num_tx_power; count++) {
+				tpe_ptr->tx_power[count] = reg_power * 2;
 				pe_debug("non-psd default TPE %d %d",
 					 count, tpe_ptr->tx_power[count]);
 			}
-
 			num_tpe_ies++;
 			tpe_ptr++;
 		}
@@ -389,9 +382,9 @@ populate_dot11f_tx_power_env(struct mac_context *mac,
 		if (reg_power) {
 			bw_val = wlan_reg_get_bw_value(chan_width);
 			bw_threshold = 20;
-			power_for_bss = reg_power + 13;
+			power_for_bss = eirp_power + 13;
 
-			while ((eirp_power > power_for_bss) &&
+			while ((reg_power > power_for_bss) &&
 			       (bw_threshold < bw_val)) {
 				bw_threshold = 2 * bw_threshold;
 				power_for_bss += 3;
@@ -405,20 +398,13 @@ populate_dot11f_tx_power_env(struct mac_context *mac,
 				tpe_ptr->max_tx_pwr_interpret = 2;
 				tpe_ptr->max_tx_pwr_category = 1;
 				tpe_ptr->num_tx_power = num_tx_power;
-				for (count = 0, bw_val = 20;
-				     count < num_tx_power;
-				     count++, bw_val += 20) {
-					if (bw_val >= bw_threshold)
-						tpe_ptr->tx_power[count] =
-								eirp_power * 2;
-					else
-						tpe_ptr->tx_power[count] =
-								reg_power * 2;
+				for (count = 0; count < num_tx_power; count++) {
+					tpe_ptr->tx_power[count] =
+							reg_power * 2;
 					pe_debug("non-psd subord TPE %d %d",
 						 count,
 						 tpe_ptr->tx_power[count]);
 				}
-
 				num_tpe_ies++;
 				tpe_ptr++;
 			}
@@ -452,11 +438,10 @@ populate_dot11f_tx_power_env(struct mac_context *mac,
 							&psd_tpe,
 							&reg_power,
 							&eirp_power);
-			tpe_ptr->tx_power[count] = reg_power * 2;
+			tpe_ptr->tx_power[count] = eirp_power * 2;
 			pe_debug("psd default TPE %d %d",
 				 count, tpe_ptr->tx_power[count]);
 		}
-
 		num_tpe_ies++;
 		tpe_ptr++;
 
@@ -467,7 +452,7 @@ populate_dot11f_tx_power_env(struct mac_context *mac,
 						      &reg_power,
 						      &eirp_power);
 
-		if (reg_power) {
+		if (eirp_power) {
 			tpe_ptr->present = 1;
 			tpe_ptr->max_tx_pwr_count = max_tx_pwr_count_psd;
 			tpe_ptr->max_tx_pwr_interpret = 3;
@@ -483,16 +468,14 @@ populate_dot11f_tx_power_env(struct mac_context *mac,
 							&psd_tpe,
 							&reg_power,
 							&eirp_power);
-				tpe_ptr->tx_power[count] = reg_power * 2;
+				tpe_ptr->tx_power[count] = eirp_power * 2;
 				pe_debug("psd subord TPE %d %d",
 					 count, tpe_ptr->tx_power[count]);
 			}
-
 			num_tpe_ies++;
 			tpe_ptr++;
 		}
 	}
-
 	*num_tpe = num_tpe_ies;
 }
 
