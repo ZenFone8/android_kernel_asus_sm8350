048f5c0828f188e5dfa7db15516659d82fb5f525
Cosmin Tanislav <demonsingur@gmail.com>
2022-02-09 16:48:03 +0200

    usb: dwc3: do not check direction
    Change-Id: If77de7f7372a4bcdfd2baccdf7eaed0daef0e388

--- drivers/usb/dwc3/dwc3-msm.c
+++ drivers/usb/dwc3/dwc3-msm.c
@@ -5062,6 +5062,7 @@ static int dwc3_msm_probe(struct platform_device *pdev)
 	}
 
 	if (!mdwc->role_switch && !mdwc->extcon) {
+#ifndef CONFIG_MACH_ASUS
 		switch (dwc->dr_mode) {
 		case USB_DR_MODE_OTG:
 			if (of_property_read_bool(node,
@@ -5091,6 +5092,7 @@ static int dwc3_msm_probe(struct platform_device *pdev)
 		}
 
 		dwc3_ext_event_notify(mdwc);
+#endif
 	}
 
 	device_create_file(&pdev->dev, &dev_attr_orientation);
