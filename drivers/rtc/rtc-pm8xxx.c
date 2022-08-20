f93cd86d1cdc09e16fd0fc251435831260d97538
Cosmin Tanislav <demonsingur@gmail.com>
2022-08-20 18:37:22 +0300

    rtc: pm8xxx: import asus changes
    Change-Id: I37ff21399d2f56d8166d0c44867dd20c2ae3f8c9

--- drivers/rtc/rtc-pm8xxx.c
+++ drivers/rtc/rtc-pm8xxx.c
@@ -171,6 +171,53 @@ rtc_rw_fail:
 	return rc;
 }
 
+#ifdef CONFIG_MACH_ASUS
+struct pm8xxx_rtc *asus_rtc_dd;
+unsigned long asus_qpnp_rtc_read_time(void)
+{
+	int rc=-1;
+	u8 value[NUM_8_BIT_RTC_REGS];
+	unsigned long secs;
+	unsigned int reg;
+	const struct pm8xxx_rtc_regs *regs = asus_rtc_dd->regs;
+
+	if(!asus_rtc_dd){
+		pr_err("asus rtc add is NULL!\n");
+		return rc;
+	}
+
+	rc = regmap_bulk_read(asus_rtc_dd->regmap, regs->read, value, sizeof(value));
+	if (rc) {
+		pr_err("RTC read data register failed\n");
+		return rc;
+	}
+
+	/*
+	 * Read the LSB again and check if there has been a carry over.
+	 * If there is, redo the read operation.
+	 */
+	rc = regmap_read(asus_rtc_dd->regmap, regs->read, &reg);
+	if (rc < 0) {
+		pr_err("RTC read data register failed\n");
+		return rc;
+	}
+
+	if (unlikely(reg < value[0])) {
+		rc = regmap_bulk_read(asus_rtc_dd->regmap, regs->read,
+				      value, sizeof(value));
+		if (rc) {
+			pr_err("RTC read data register failed\n");
+			return rc;
+		}
+	}
+
+	secs = value[0] | (value[1] << 8) | (value[2] << 16) | (value[3] << 24);
+
+	return secs;
+}
+EXPORT_SYMBOL(asus_qpnp_rtc_read_time);
+#endif
+
 static int pm8xxx_rtc_read_time(struct device *dev, struct rtc_time *tm)
 {
 	int rc;
@@ -486,6 +533,11 @@ static const struct of_device_id pm8xxx_id_table[] = {
 };
 MODULE_DEVICE_TABLE(of, pm8xxx_id_table);
 
+#ifdef CONFIG_MACH_ASUS
+bool rtc_probe_done = false;
+EXPORT_SYMBOL(rtc_probe_done);
+#endif
+
 static int pm8xxx_rtc_probe(struct platform_device *pdev)
 {
 	int rc;
@@ -548,6 +600,11 @@ static int pm8xxx_rtc_probe(struct platform_device *pdev)
 
 	if (of_property_read_bool(pdev->dev.of_node, "disable-alarm-wakeup"))
 		device_set_wakeup_capable(&pdev->dev, false);
+
+#ifdef CONFIG_MACH_ASUS
+	asus_rtc_dd = rtc_dd;
+	rtc_probe_done = true;
+#endif
 
 	dev_dbg(&pdev->dev, "Probe success !!\n");
 
